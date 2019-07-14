1d8869af0014461c30c681d3d843bce642905e6c
Elliott Esponda <e_e102@txstate.edu>
2019-07-13 22:26:22 -0500

    changes some of the boiler plate code

--- deploy.js
+++ deploy.js
@@ -6,7 +6,6 @@ const provider = new HDWalletProvider(
     '--replace me--',// your 12 word mneumonic goes here
     'https://rinkeby.infura.io/############7'//your enfura endpoint goes here
 );
-console.log('we get here!');
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -15,7 +14,7 @@ const deploy = async () => {
     console.log("Attempting to deploy from account ", accounts[0]);
 
     const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: '0x' + bytecode, arguments: ['Hi there!']})
+        .deploy({data: '0x' + bytecode })
         .send({ gas:"1000000", from: accounts[0] });
 
     console.log('contract deployed to ', result.options.address);
